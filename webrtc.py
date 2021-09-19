ccca0814b1e60cb040b2147445c36280abdad3e1
user <user@server.localnet.jp>
2021-09-19 15:02:27 +0900

    modify webrtc.py

--- webrtc.py
+++ webrtc.py
@@ -3,49 +3,49 @@ import time
 import signal
 import threading
 import os
+import logging
+import logging.config
+import daemon
 from selenium import webdriver
-from daemon import DaemonContext
-from lockfile.pidlockfile import PIDLockFile
 
-class WebRTC(threading.Thread):
+class WebRTC:
     """
     Web Real-Time Communication
 
     Attributes
     ----------
+    status : boolean
+    logger : logging.logger
+    max_wait_sec : int
     event : threading.Event
     """
 
-    def __init__(self, max_wait_sec=3600):
+    def __init__(self, config_name, max_wait_sec=3600):
         """
         constructor
 
         Parameters
         ----------
+        config_name : str
+            config name of logging
         max_wait_sec : int
             waiting time for repeating url access
             default: 3600 [sec]
         """
         super().__init__()
         self.status= True
+        self.logger = logging.getLogger(config_name)
         self.max_wait_sec = max_wait_sec
         self.event = threading.Event()
 
-    def change_status(self, signum, frame):
+    def update_status(self):
         """
-        change status
-
-        Parameters
-        ----------
-        signum : int
-            signal number
-        frame : str
-            frame information
+        update status
         """
         self.status = False
         self.event.set()
 
-    def run(self):
+    def execute(self, username, password):
         """
         thread function
         """
@@ -64,9 +64,9 @@ class WebRTC(threading.Thread):
         time.sleep(wait_time_sec)
         # enter username and password
         username_field = driver.find_element_by_name('username')
-        username_field.send_keys(os.getenv('WEBRTC_USERNAME'))
+        username_field.send_keys(username)
         password_field = driver.find_element_by_name('password')
-        password_field.send_keys(os.getenv('WEBRTC_PASSWORD'))
+        password_field.send_keys(password)
         # login process
         login_btn = driver.find_element_by_id('btn-login')
         login_btn.click()
@@ -77,27 +77,93 @@ class WebRTC(threading.Thread):
         while self.status:
             driver.get(access_url)
             soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
-            print(soup.h3)
+            self.logger.info('{}: {}'.format(username, soup.h3.text))
             self.event.wait(self.max_wait_sec)
             self.event.clear()
 
         driver.quit()
 
+class ProcessStatus():
+    """
+    Process Status
+    Attributes
+    ----------
+    __status : bool
+        True  : running
+        False : stopped
+    """
+
+    def __init__(self):
+        """
+        constructor
+        """
+        self.__status = True
+
+    def change_status(self, signum, frame):
+        """
+        change status
+        Parameters
+        ----------
+        signum : int
+            signal number
+        frame : str
+            frame information
+        """
+        self.__status = False
+
+    def get_status(self):
+        """
+        get current status
+        """
+        return self.__status
+
 if __name__ == '__main__':
     # initialization
-    max_wait_sec = 60 * 60
-    webrtc = WebRTC(max_wait_sec=max_wait_sec)
-    signal_map = {
-        signal.SIGINT, webrtc.change_status,
-        signal.SIGTERM, webrtc.change_status,
-    }
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'infoFormat': {
+                'format': '[%(asctime)s %(levelname)s] %(name)s %(message)s',
+                'datefmt': '%Y/%m/%d %H:%M:%S'
+            }
+        },
+        'handlers': {
+            'timeRotate': {
+                'class': 'logging.handlers.TimedRotatingFileHandler',
+                'formatter': 'infoFormat',
+                'filename': '/var/log/webrtc/status.log',
+                'when': 'W2',
+                'backupCount': 5
+            },
+            'consoleHandler': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'infoFormat'
+            }
+        },
+        'loggers': {
+            'webrtc': {
+                'level': 'INFO',
+                'handlers': ['timeRotate', 'consoleHandler']
+            }
+        }
+    })
+    process_status = ProcessStatus()
+    signal.signal(signal.SIGINT, process_status.change_status)
+    signal.signal(signal.SIGTERM, process_status.change_status)
+    pidfile = daemon.pidfile.PIDLockFile('/var/run/webrtc.pid')
 
-    with DaemonContext(pidfile=PIDLockFile('/var/run/webrtc.pid'), signal_map=signal_map) as context:
-        webrtc.start()
+    if not pidfile.is_locked():
+        with daemon.DaemonContext(working_directory=os.getcwd(), pidfile=pidfile):
+            max_wait_sec = 60 * 60
+            webrtc = WebRTC('webrtc', max_wait_sec=max_wait_sec)
+            thread = threading.Thread(target=webrtc.execute, args=(os.getenv('WEBRTC_USERNAME'), os.getenv('WEBRTC_PASSWORD'),))
+            thread.start()
 
-        # main loop
-        while webrtc.status:
-            time.sleep(1)
+            # main loop
+            while process_status.get_status():
+                time.sleep(3)
 
-        # finalization
-        webrtc.join()
+            # finalization
+            webrtc.update_status()
+            thread.join()
