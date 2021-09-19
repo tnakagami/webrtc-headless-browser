5a9b2e5137e088102404afd223550957e62a7a7a
user <user@server.localnet.jp>
2021-09-19 14:31:22 +0900

    update webrtc.py

--- webrtc.py
+++ webrtc.py
@@ -4,6 +4,8 @@ import signal
 import threading
 import os
 from selenium import webdriver
+from daemon import DaemonContext
+from lockfile.pidlockfile import PIDLockFile
 
 class WebRTC(threading.Thread):
     """
@@ -11,13 +13,10 @@ class WebRTC(threading.Thread):
 
     Attributes
     ----------
-    process_status : ProcessStatus
-        instance of ProcessStatus
-    driver : webdriver
     event : threading.Event
     """
 
-    def __init__(self, process_status, max_wait_sec=3600):
+    def __init__(self, max_wait_sec=3600):
         """
         constructor
 
@@ -30,69 +29,9 @@ class WebRTC(threading.Thread):
             default: 3600 [sec]
         """
         super().__init__()
-        self.process_status = process_status
-        chrome_option = webdriver.ChromeOptions()
-        chrome_option.add_argument('--headless')
-        chrome_option.add_argument('--disable-gpu')
-        chrome_option.add_argument('--autoplay-policy=no-user-gesture-required')
+        self.status= True
         self.max_wait_sec = max_wait_sec
-        self.driver = webdriver.Chrome(options=chrome_option)
         self.event = threading.Event()
-
-    def is_event_set(self):
-        """
-        set event status
-        """
-        self.event.set()
-
-    def run(self):
-        """
-        thread function
-        """
-        base_url = os.getenv('WEBRTC_BASE_URL')
-        wait_time_sec = 3
-
-        # access login page
-        login_url = '{}/index.php'.format(base_url)
-        self.driver.get(login_url)
-        time.sleep(wait_time_sec)
-        # enter username and password
-        username_field = self.driver.find_element_by_name('username')
-        username_field.send_keys(os.getenv('WEBRTC_USERNAME'))
-        password_field = self.driver.find_element_by_name('password')
-        password_field.send_keys(os.getenv('WEBRTC_PASSWORD'))
-        # login process
-        login_btn = self.driver.find_element_by_id('btn-login')
-        login_btn.click()
-        time.sleep(wait_time_sec)
-
-        # access dashboard
-        access_url = '{}/index.php?display=dashboard'.format(base_url)
-        while self.process_status.get_status():
-            self.driver.get(access_url)
-            soup = bs4.BeautifulSoup(self.driver.page_source, 'html.parser')
-            print(soup.h3)
-            self.event.wait(self.max_wait_sec)
-            self.event.clear()
-
-        self.driver.quit()
-
-class ProcessStatus():
-    """
-    Process Status
-
-    Attributes
-    ----------
-    __status : bool
-        True  : running
-        False : stopped
-    """
-
-    def __init__(self):
-        """
-        constructor
-        """
-        self.__status = True
 
     def change_status(self, signum, frame):
         """
@@ -105,28 +44,62 @@ class ProcessStatus():
         frame : str
             frame information
         """
-        self.__status = False
+        self.status = False
+        self.event.set()
 
-    def get_status(self):
+    def run(self):
         """
-        get current status
+        thread function
         """
-        return self.__status
+        base_url = os.getenv('WEBRTC_BASE_URL')
+        wait_time_sec = 3
+        # setup driver
+        chrome_option = webdriver.ChromeOptions()
+        chrome_option.add_argument('--headless')
+        chrome_option.add_argument('--disable-gpu')
+        chrome_option.add_argument('--autoplay-policy=no-user-gesture-required')
+        driver = webdriver.Chrome(options=chrome_option)
+
+        # access login page
+        login_url = '{}/index.php'.format(base_url)
+        driver.get(login_url)
+        time.sleep(wait_time_sec)
+        # enter username and password
+        username_field = driver.find_element_by_name('username')
+        username_field.send_keys(os.getenv('WEBRTC_USERNAME'))
+        password_field = driver.find_element_by_name('password')
+        password_field.send_keys(os.getenv('WEBRTC_PASSWORD'))
+        # login process
+        login_btn = driver.find_element_by_id('btn-login')
+        login_btn.click()
+        time.sleep(wait_time_sec)
+
+        # access dashboard
+        access_url = '{}/index.php?display=dashboard'.format(base_url)
+        while self.status:
+            driver.get(access_url)
+            soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
+            print(soup.h3)
+            self.event.wait(self.max_wait_sec)
+            self.event.clear()
+
+        driver.quit()
 
 if __name__ == '__main__':
-    process_status = ProcessStatus()
-    signal.signal(signal.SIGINT, process_status.change_status)
-    signal.signal(signal.SIGTERM, process_status.change_status)
-
     # initialization
     max_wait_sec = 60 * 60
-    webrtc = WebRTC(process_status, max_wait_sec=max_wait_sec)
-    webrtc.start()
+    webrtc = WebRTC(max_wait_sec=max_wait_sec)
+    signal_map = {
+        signal.SIGINT, webrtc.change_status,
+        signal.SIGTERM, webrtc.change_status,
+    }
 
-    # main loop
-    while process_status.get_status():
-        time.sleep(3)
+    with DaemonContext(pidfile=PIDLockFile('/var/run/webrtc.pid'), signal_map=signal_map) as context:
+        webrtc.start()
 
-    # finalization
-    webrtc.is_event_set()
-    webrtc.join()
+        # main loop
+        while webrtc.status:
+            time.sleep(1)
+
+        # finalization
+        webrtc.join()
